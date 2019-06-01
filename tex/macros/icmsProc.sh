f884da38c320a3df1be851d6e916e25362be3218
Mohsen Banan <mohsen.banan2@t-mobile.com>
2019-06-01 10:42:55 -0700

    Added basic bystar .sty files.

---
+++ tex/macros/icmsProc.sh
@@ -0,0 +1,397 @@
+#!/bin/bash
+
+IimBriefDescription="iimsProc.sh: A scattered IIM extensions based on seedIimsProc.sh"
+
+ORIGIN="
+* Revision And Libre-Halaal CopyLeft -- Part Of ByStar -- Best Used With Blee
+"
+
+####+BEGIN: bx:dblock:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+minimal"
+typeset RcsId="$Id: iimsProc.leaf.sh,v 1.2 2017-07-06 01:17:45 lsipusr Exp $"
+# *CopyLeft*
+#  This is a Halaal Poly-Existential. See http://www.freeprotocols.org
+
+####+END:
+
+__author__="
+* Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
+"
+####+BEGIN: bx:dblock:lsip:bash:seed-spec :types "seedIimsProc.sh"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/opt/public/osmt/bin/seedIimsProc.sh]] | 
+"
+FILE="
+*  /This File/ :: /de/bx/nne/dev-py/iimsPkgs/remoteMailActions/envelopeDsn/iimsProc.sh 
+"
+if [ "${loadFiles}" == "" ] ; then
+    /opt/public/osmt/bin/seedIimsProc.sh -l $0 "$@" 
+    exit $?
+fi
+####+END:
+
+_CommentBegin_
+####+BEGIN: bx:dblock:global:file-insert-cond :cond "./blee.el" :file "/libre/ByStar/InitialTemplates/software/plusOrg/dblock/inserts/topControls.org"
+*  /Controls/ ::  [[elisp:(org-cycle)][| ]]  [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[file:Panel.org][Panel]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(delete-other-windows)][(1)]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
+** /Version Control/ ::  [[elisp:(call-interactively (quote cvs-update))][cvs-update]]  [[elisp:(vc-update)][vc-update]] | [[elisp:(bx:org:agenda:this-file-otherWin)][Agenda-List]]  [[elisp:(bx:org:todo:this-file-otherWin)][ToDo-List]] 
+
+####+END:
+_CommentEnd_
+
+_CommentBegin_
+*      ================
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]] CONTENTS-LIST ################
+*  [[elisp:(org-cycle)][| ]]  Info          :: *[Current-Info:]*  Status, Notes (Tasks/Todo Lists, etc.) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *Seed Extensions*
+_CommentEnd_
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  IIM Profile / Characteristic Definitions    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+# Types are defined as associative arrays in /opt/public/osmt/lib/visLib.sh
+
+G_iimGroupingType=${IimGroupingType[pkged]}
+
+G_myPanel=$(FN_prefix ${G_myName})-Panel.org
+
+G_iimCmndParts="
+${IimCmndParts[common]}
+"
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Examples      :: Examples Hook Post [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  examplesHookPost    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function examplesHookPost {
+cat  << _EOF_
+$( examplesSeperatorChapter "*Extentions* (examplesHookPost)" )
+${G_myName} ${extraInfo} -i panelPreps
+${G_myName} ${extraInfo} -i iimsPkgPreps   # from visLib.sh sets up ./var/iimsPkg/iimsIn/paramsFp
+$( examplesSeperatorChapter "Some Local IIM Extensions" )
+${G_myName} ${extraInfo} -i defaultsTargetsAndParamsSet
+${G_myName} ${extraInfo} -i pkgInit $(cat _selectedIim)
+${G_myName} ${extraInfo} -i pkgInfo
+$( examplesSeperatorChapter "*Templates Development*" )
+----  Initial Templates Development ----
+diff ./iimsProc.sh  /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/iimsProc.leaf.sh
+cp ./iimsProc.sh  /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/iimsProc.leaf.sh
+cp  /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/iimsProc.leaf.sh ./iimsProc.sh 
+----  Initial Templates Development ----
+diff ./Panel.org /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/common/beginPanel.org
+cp ./Panel.org /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/common/beginPanel.org
+cp /libre/ByStar/InitialTemplates/begin/templates/purposed/iims/bash/common/beginPanel.org ./Panel.org 
+_EOF_
+ return
+}
+
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_pkgInfo    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_pkgInfo {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local bxtObject=$(cat _objectType_)
+
+    if [ ! -f _iimsList ] ; then
+	EH_problem ""
+	lpReturn
+    fi
+
+    local iimsList=$(cat _iimsList)
+
+    ANT_raw "iimsList=${iimsList}"
+
+    local iimLanguage=""
+    local thisIim=""
+    for thisIim in ${iimsList}; do
+	iimLanguage=$( ${thisIim} -i iimLanguage)
+	echo "${thisIim} -- ${iimLanguage}"
+	
+	if [ "${iimLanguage}" == "bash" ] ; then
+	    opDo ${thisIim} -i iimGroupingType
+	    opDo ${thisIim} -i iimCmndPartList
+	elif [ "${iimLanguage}" == "python" ] ; then
+	    echo "NOTYET -- IIM-Profile can be retrieved with -i iim.groupingType etc"	    
+	    EH_problem "python ${thisIim} -- Notyet"
+	else
+	    EH_oops "${thisIim}"
+	    continue
+	fi
+    done
+
+    case "${bxtObject}" in
+	"bxt.iim.bash.pkged")
+	    ANT_cooked "Processed bxtObject=${bxtObject}"	    
+	    ;;
+	*)
+	    ANT_cooked "Unprocessed bxtObject=${bxtObject}"
+	    ;;
+    esac
+
+
+    lpReturn
+  }
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_varIimsPkgPrep    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_varIimsPkgPrep {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+    
+    opDo fileParamManage.py -i fileParamWrite ./iimsIn/control iimsPkgLabel  $(FN_nonDirsPart $(pwd))
+    opDo fileParamManage.py -i fileParamWrite ./iimsIn/control dateVer "uninitialized"    
+
+    
+    if [ $# -eq 0 ] ; then
+	iimsList=$(cat _iimsList)
+    else
+	iimsList="$@"
+    fi
+
+    opDo FN_dirCreatePathIfNotThere ./tmp    
+
+    local iimLanguage=""
+    local thisIim=""
+    for thisIim in ${iimsList}; do
+	iimLanguage=$( ${thisIim} -i iimLanguage)
+	if [ "${iimLanguage}" == "bash" ] ; then
+	    opDo ${thisIim} -h -v -n showRun -i pkgedPanelPreps	    
+	elif [ "${iimLanguage}" == "python" ] ; then
+	    EH_problem "python ${thisIim} -- Notyet"
+	else
+	    EH_oops ""
+	    continue
+	fi
+    done
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_pkgInit    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_pkgInit {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    local iimsList=""
+    
+    if [ $# -eq 0 ] ; then
+	iimsList=$(cat _iimsList)
+    else
+	iimsList="$@"
+    fi
+
+    opDo FN_dirCreatePathIfNotThere ./tmp    
+
+    local iimLanguage=""
+    local thisIim=""
+    for thisIim in ${iimsList}; do
+	iimLanguage=$( ${thisIim} -i iimLanguage)
+	if [ "${iimLanguage}" == "bash" ] ; then
+	    opDo ${thisIim} -h -v -n showRun -i pkgedPanelPreps	    
+	elif [ "${iimLanguage}" == "python" ] ; then
+	    EH_problem "python ${thisIim} -- Notyet"
+	else
+	    EH_oops ""
+	    continue
+	fi
+    done
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_pkgInitObsoleted    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_pkgInitObsoleted {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    if [ -d "./tmp" ] ; then
+	opDo mv ./tmp ./tmp.${dateTag}
+    fi
+    FN_dirCreatePathIfNotThere ./tmp
+    opDo fileParamManage.py -i fileParamWrite ./tmp dateVer "uninitialized"
+
+    if [ -d "./iimsIn" ] ; then
+	opDo mv ./iimsIn ./iimsIn.${dateTag}
+    fi
+    FN_dirCreatePathIfNotThere ./iimsIn
+    FN_dirCreatePathIfNotThere ./iimsIn/control
+    opDo fileParamManage.py -i fileParamWrite ./iimsIn/control iimsPkgLabel  $(FN_nonDirsPart $(pwd))
+    opDo fileParamManage.py -i fileParamWrite ./iimsIn/control dateVer "uninitialized"    
+
+    FN_dirCreatePathIfNotThere ./iimsIn/targets
+    FN_dirCreatePathIfNotThere ./iimsIn/proxies    
+
+    if [ -d "./iimsOut" ] ; then
+	opDo mv ./iimsOut ./iimsOut.${dateTag}
+    fi
+    FN_dirCreatePathIfNotThere ./iimsOut
+
+    opDo vis_defaultsTargetsAndParamsSet    
+
+    lpReturn
+
+    opDo vis_selectionsPanelPrep
+
+    opDo iimsProc.sh -h -v -n showRun  -p liTargetsDotpy=/de/bx/current/district/librecenter/tiimi/targets/bxp/tList/ts-librecenter-localhost.py -p liParamsDotpy=/de/bx/current/district/librecenter/tiimi/targets/bxp/paramList/bxpUsageParams.py -i effectiveLisSet    
+}
+
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_pkgDistClean    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_pkgDistClean {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+NOTYET, 
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+    #describeF
+
+    opDo FN_dirRemoveIfThere ./var
+
+    opDo FN_dirRemoveIfThere ./tmp
+
+    lpReturn    
+
+    #opDo vis_effectiveLisClean 
+    #opDo FN_fileSymlinkRemoveIfThere ./liTargets.py
+    #opDo FN_fileSymlinkRemoveIfThere ./liParams.py
+
+    #opDo vis_selectionsPanelClean    
+    #opDo FN_fileSymlinkRemoveIfThere ./panelSelects
+
+    #opDo FN_dirRemoveIfThere ./output  ./outputFiles ./outputFiles__main__
+    
+    #opDo FN_dirRemoveIfThere ./data_cache
+
+    #opDo FN_dirRemoveIfThere ./inputFiles
+
+    #opDo FN_dirRemoveIfThere ./iimsOut  
+
+    #opDo FN_fileRmIfThere ./eml-*.org                 
+    
+    lpReturn
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_panelPreps    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_panelPreps {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    typeset tmpFile=$( FN_tempFile )
+
+    typeset selectedIim=$( cat ./_selectedIim )
+
+    if [ -z ${selectedIim} ] ; then
+	ANT_raw "Missing _selectedIim -- panelPreps skipped"
+	lpReturn
+    fi
+
+    cat  ./Panel.org  | sed  \
+	-e "s:@selectedIim@:${selectedIim}:" \
+        >> ${tmpFile}
+
+    opDo mv ${tmpFile} ./Panel.org
+    
+    lpReturn
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] [[elisp:(beginning-of-buffer)][Top]] [[elisp:(delete-other-windows)][(1)]] || IIF       ::  vis_defaultsTargetsAndParamsSet    [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_defaultsTargetsAndParamsSet {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    #
+    # To be customized on a per pkg basis
+    #
+
+    typeset objectType=$(cat _objectType_)
+    
+    if [ -z "${objectType}" ] ; then
+	EH_problem "Missing _objectType_"
+	lpReturn 101
+    fi
+    
+    case ${objectType} in
+	"bxt.py.iim.to")
+	    opDo vis_selectionsPanelPrep	    
+	    opDo iimsProc.sh -h -v -n showRun  -p liTargetsDotpy=/de/bx/current/district/librecenter/tiimi/targets/bxp/tList/ts-librecenter-localhost.py -p liParamsDotpy=/de/bx/current/district/librecenter/tiimi/targets/bxp/paramList/bxpUsageParams.py -i effectiveLisSet    	    
+	    ;;
+	"bxt.py.iim.polySon")
+	    opDo vis_selectionsPanelPrep	    
+	    ;;
+	"bxt.py.iim")
+	    doNothing
+	    ;;
+	*)
+	    ANT_raw "Unsupported objectType=${objectType}"
+	    ;;	    
+    esac
+    
+    lpReturn
+}
+
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *End Of Editable Text*
+_CommentEnd_
+
+####+BEGIN: bx:dblock:bash:end-of-file :types ""
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Common        ::  /[dblock] -- End-Of-File Controls/ [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+#+STARTUP: showall
+#local variables:
+#major-mode: sh-mode
+#fill-column: 90
+# end:
+####+END:
---
+++ tex/macros/texExtras.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+IimBriefDescription="NOTYET: Short Description Of The Module"
+
+ORIGIN="
+* Revision And Libre-Halaal CopyLeft -- Part Of ByStar -- Best Used With Blee
+"
+
+####+BEGIN: bx:dblock:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+brief"
+typeset RcsId="$Id: dblock-iim-bash.el,v 1.4 2017-02-08 06:42:32 lsipusr Exp $"
+# *CopyLeft*
+# Copyright (c) 2011 Neda Communications, Inc. -- http://www.neda.com
+# See PLPC-120001 for restrictions.
+# This is a Halaal Poly-Existential intended to remain perpetually Halaal.
+####+END:
+
+__author__="
+* Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
+"
+
+
+####+BEGIN: bx:dblock:lsip:bash:seed-spec :types "seedActions.bash"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/opt/public/osmt/bin/seedActions.bash]] | 
+"
+FILE="
+*  /This File/ :: /bisos/apps/bxtex/tex/macros/texExtras.sh 
+"
+if [ "${loadFiles}" == "" ] ; then
+    /opt/public/osmt/bin/seedActions.bash -l $0 "$@" 
+    exit $?
+fi
+####+END:
+
+_CommentBegin_
+####+BEGIN: bx:dblock:global:file-insert-cond :cond "./blee.el" :file "/libre/ByStar/InitialTemplates/software/plusOrg/dblock/inserts/topControls.org"
+*      ================
+*  /Controls/ ::  [[elisp:(org-cycle)][| ]]  [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[file:Panel.org][Panel]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(delete-other-windows)][(1)]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
+** /Version Control/ ::  [[elisp:(call-interactively (quote cvs-update))][cvs-update]]  [[elisp:(vc-update)][vc-update]] | [[elisp:(bx:org:agenda:this-file-otherWin)][Agenda-List]]  [[elisp:(bx:org:todo:this-file-otherWin)][ToDo-List]] 
+####+END:
+_CommentEnd_
+
+_CommentBegin_
+*      ================
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]] CONTENTS-LIST ################
+*  [[elisp:(org-cycle)][| ]]  Notes         :: *[Current-Info:]*  Status, Notes (Tasks/Todo Lists, etc.) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_moduleDescription {  cat  << _EOF_
+*  [[elisp:(org-cycle)][| ]]  Xrefs         :: *[Related/Xrefs:]*  <<Xref-Here->>  -- External Documents  [[elisp:(org-cycle)][| ]]
+**  [[elisp:(org-cycle)][| ]]  Panel        :: [[file:/libre/ByStar/InitialTemplates/activeDocs/bxServices/versionControl/fullUsagePanel-en.org::Xref-VersionControl][Panel Roadmap Documentation]] [[elisp:(org-cycle)][| ]]
+*  [[elisp:(org-cycle)][| ]]  Info          :: *[Module Description:]* [[elisp:(org-cycle)][| ]]
+
+_EOF_
+}
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *Seed Extensions*
+_CommentEnd_
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Imports       :: Prefaces (Imports/Libraries) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+. ${opBinBase}/opAcctLib.sh
+. ${opBinBase}/opDoAtAsLib.sh
+. ${opBinBase}/lpParams.libSh
+. ${opBinBase}/lpReRunAs.libSh
+
+
+texExtrasBaseDir="/usr/share/texmf/tex/extras"
+bisos_appsTexExtras="/bisos/apps/bxtex/tex/macros"
+
+# PRE parameters
+
+baseDir=""
+
+function G_postParamHook {
+     return 0
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Examples      :: Examples [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+function vis_examples {
+    typeset extraInfo="-h -v -n showRun"
+    #typeset extraInfo=""
+    typeset runInfo="-p ri=lsipusr:passive"
+
+    typeset examplesInfo="${extraInfo} ${runInfo}"
+
+    visLibExamplesOutput ${G_myName} 
+  cat  << _EOF_
+$( examplesSeperatorTopLabel "${G_myName}" )
+$( examplesSeperatorChapter "Initial Setup" )
+$( examplesSeperatorSection "/usr/share/texmf/tex/extras" )
+${G_myName} ${extraInfo} -i extrasEndShow
+${G_myName} ${extraInfo} -i extrasEnd_osmt2_verify
+${G_myName} ${extraInfo} -i extrasEnd_osmt2_set
+${G_myName} ${extraInfo} -i extrasEnd_bisos1_verify
+${G_myName} ${extraInfo} -i extrasEnd_bisos1_set
+${G_myName} ${extraInfo} -i texhash
+_EOF_
+}
+
+noArgsHook() {
+  vis_examples
+}
+
+
+function vis_extrasEndShow {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo readlink ${texExtrasBaseDir}
+
+    lpReturn
+}
+
+
+function vis_extrasEndSet_osmt2 {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo readlink -f ${texExtrasBaseDir}
+
+    lpReturn
+}
+
+
+function vis_extrasEnd_bisos1_verify {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    local texExtrasBaseDirEndPath=$(readlink -f "${texExtrasBaseDir}")
+    local bisos_appsTexExtrasEndPath=$(readlink -f "${bisos_appsTexExtras}")
+    local retVal=0
+
+    if [ ! -d "${bisos_appsTexExtras}" ] ; then 
+	EH_problem "Missing ${bisos_appsTexExtras}"
+	lpReturn 1
+    fi
+
+    if [ -z "${texExtrasBaseDirEndPath}" ] ; then
+	EH_problem "Missing ${texExtrasBaseDir}"
+	lpReturn 1
+    fi
+
+    if [ -z "${texExtrasBaseDirEndPath}" ] ; then
+	EH_problem "Missing ${bisos_appsTexExtras}"
+	lpReturn 1
+    fi
+
+    if [ "${texExtrasBaseDirEndPath}" != "${bisos_appsTexExtrasEndPath}" ] ; then
+	EH_problem "EndPath Mismatch"
+	retVal=1
+    else
+	ANT_raw "Verified -- EndPath is as expected"
+    fi
+    
+    opDo ls -ld "${texExtrasBaseDirEndPath}" "${bisos_appsTexExtrasEndPath}"
+
+    lpReturn ${retVal}
+}
+
+
+
+function vis_extrasEnd_bisos1_set {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    if vis_reRunAsRoot ${G_thisFunc} $@ ; then lpReturn ${globalReRunRetVal}; fi;
+
+    local texExtrasBaseDirEndPath=$(readlink -f "${texExtrasBaseDir}")
+    local bisos_appsTexExtrasEndPath=$(readlink -f "${bisos_appsTexExtras}")
+    local retVal=0
+    local gotVal=0
+
+    vis_extrasEnd_bisos1_verify  > /dev/null 2>&1 
+    gotVal=$?
+
+    if [ ${gotVal} == 0 ] ; then
+	ANT_raw "Already Set:"
+	opDo vis_extrasEnd_bisos1_verify
+	lpReturn 0
+    fi
+
+    # So, the ends don't match
+
+    #
+    # Make sure /bisos/apps/bxtex exists
+    #
+
+    if [ ! -d "${bisos_appsTexExtras}" ] ; then 
+	EH_problem "Missing ${bisos_appsTexExtras}"
+	lpReturn 1
+    fi
+
+    #
+    # Do the link update.
+    #
+    
+    opDo FN_fileSymlinkUpdate ${bisos_appsTexExtras}  ${texExtrasBaseDir}
+    #
+    # Can also be done with:
+    #
+    # TEXINPUTS=.:/opt/public/osmt/share/texmf/tex/extras::
+    #
+    
+    opDo vis_extrasEnd_bisos1_verify    
+    
+    lpReturn ${retval}
+}
+
+
+
+function vis_texhash {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo sudo texhash
+ }
+
+
+
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *End Of Editable Text*
+_CommentEnd_
+
+####+BEGIN: bx:dblock:bash:end-of-file :type "basic"
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Common        ::  /[dblock] -- End-Of-File Controls/ [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+#+STARTUP: showall
+#local variables:
+#major-mode: sh-mode
+#fill-column: 90
+# end:
+####+END:
